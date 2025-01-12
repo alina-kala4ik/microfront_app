ff023c3bb153e36daafcfafcfdb6569ce52a3892
kala4ik <alina.kala4ik@gmail.com>
2025-01-12 21:33:23 +0100

    Implement navigation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen((location) => {
+            onNavigate(location);
+        })
+    }
+
+
     ReactDOM.render(
-        <App />,
+        <App history={history} />,
         el
     );
+
+    return {
+        onParentNavigate({ pathname: nextLocation }) {
+            const { pathname } = history.location;
+
+            if(pathname !== nextLocation) {
+                history.push(nextLocation)
+            }
+        }
+    }
 };
 
 if (process.env.NODE_ENV === 'development') {
     const wrapper = document.querySelector('#_id-marketing-dev-root');
 
     if (wrapper) {
-        mount(wrapper)
+        mount(wrapper, { defaultHistory: createBrowserHistory() })
     }
 }
 
