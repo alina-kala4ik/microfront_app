b78b2d2a786a77ce492be9c59a4c8a82b6b2e2d8
kala4ik <alina.kala4ik@gmail.com>
2025-01-14 21:00:37 +0100

    Add Progress component for improved loading UI

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,7 @@ import React, { lazy, Suspense } from 'react';
 import Header from './components/Header';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import Progress from './components/Progress';
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
@@ -15,7 +16,7 @@ const App = () => {
       <StylesProvider generateClassName={generateClassName}>
           <BrowserRouter>
               <Header />
-              <Suspense fallback={<div>Loading...</div>}>
+              <Suspense fallback={<Progress />}>
                   <Switch>
                       <Route path="/auth" component={AuthLazy} />
                       <Route path="/" component={MarketingLazy} />
