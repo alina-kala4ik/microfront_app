ef9aae0873579e3425527e577695817de73ecdd1
kala4ik <alina.kala4ik@gmail.com>
2025-01-15 17:07:30 +0100

    Add auth logic

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, {lazy, Suspense, useState} from 'react';
 import Header from './components/Header';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
@@ -12,14 +12,18 @@ const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const App = () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
       <StylesProvider generateClassName={generateClassName}>
           <BrowserRouter>
-              <Header />
+              <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
               <Suspense fallback={<Progress />}>
                   <Switch>
-                      <Route path="/auth" component={AuthLazy} />
-                      <Route path="/" component={MarketingLazy} />
+                      <Route path="/auth">
+                          <AuthLazy onSignIn={() => {setIsSignedIn(true)}} />
+                      </Route>
+                      <Route path="/" component={MarketingLazy}/>
                   </Switch>
               </Suspense>
           </BrowserRouter>
