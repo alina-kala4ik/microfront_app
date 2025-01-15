69a6baf68c274e6bc1ad2d6659722cdefbfdfb2a
kala4ik <alina.kala4ik@gmail.com>
2025-01-15 18:34:09 +0100

    app redirect logic

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,9 @@
-import React, {lazy, Suspense, useState} from 'react';
+import React, {lazy, Suspense, useState, useEffect} from 'react';
 import Header from './components/Header';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch, Router, Redirect } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Progress from './components/Progress';
+import {createBrouserHistory} from 'history';
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
@@ -12,23 +13,34 @@ const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
 const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
+const history = createBrouserHistory();
+
 const App = () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
+  useEffect(() => {
+    if (isSignedIn) {
+        history.push('/dashboard');
+    }
+  }, [isSignedIn])
+
   return (
       <StylesProvider generateClassName={generateClassName}>
-          <BrowserRouter>
+          <Router history={history}>
               <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
               <Suspense fallback={<Progress />}>
                   <Switch>
                       <Route path="/auth">
                           <AuthLazy onSignIn={() => {setIsSignedIn(true)}} />
                       </Route>
-                      <Route path="/dashboard" component={DashboardLazy}/>
+                      <Route path="/dashboard">
+                          {!isSignedIn && <Redirect to='/' />}
+                          <DashboardLazy />
+                      </Route>
                       <Route path="/" component={MarketingLazy}/>
                   </Switch>
               </Suspense>
-          </BrowserRouter>
+          </Router>
       </StylesProvider>
   )
 };
